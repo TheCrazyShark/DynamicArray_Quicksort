a348eaedbad492179ce272cc2bf6abf848d8b527
Shane Smith <sas1041@sru.edu>
2021-11-07 15:13:03 -0500

    Start of all functions

--- DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray.cpp
@@ -5,31 +5,87 @@ int partition(int* arr, int start, int end);
 void quicksort(int* arr, int start, int end);
 void sortAsc(int* arrAsc, int num);
 void sortDesc(int* arrAsc, int num);
-int calcAvg(int* arr);
+double calcAvg(int* arr, int num);
 void insert(int* arr);
 
+/***********
+    A utility function to swap two elements
+***********/ 
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+/***********
+    This function takes last element as pivot, places
+    the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+    to left of pivot and all greater elements to right of pivot 
+************/
 int partition(int* arr, int start, int end) {
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/************
+    The main function that implements QuickSort
+    arr[] --> Array to be sorted,
+    low  --> Starting index,
+    high  --> Ending index 
+ ***********/
+void quicksort(int* arr, int start, int end) {
 
 }
 
-void quicksort(int* arr, int start, int end{
+/************
 
-}
-
+************/
 void sortAsc(int *arrAsc, int num) {
     int *arrAsc = new int[num];
 
 }
 
+/************
+
+************/
 void sortDesc(int *arrDesc, int num) {
     int* arrAsc = new int[num];
 
 }
 
-int calcAvg(int* arr) {
-    int avg = 0;
+/************
+    Calculates average of array and returns it
+************/
+double calcAvg(int* arr, int num) {
+    double avg = 0, int sum = 0;
+
+    for (int i = 0; i < num; i++) {
+        sum += arr[i];
+    }
+    avg = sum / num;
 
     return avg;
+}
+
+/************
+
+************/
+void insert(int* arr) {
+
 }
 
 int main() {
@@ -49,7 +105,9 @@ int main() {
     for (x = 0; x < num; x++) {
         cout << arr[x] << " ";
     }
-    sortAsc(arr, num);
+    sortAsc(arr, num-1);
+    sortDesc(arr, num-1);
+    cout << "Average: " << calcAvg(arr, num) << endl;
 
     delete[]arr;
     return 0;
